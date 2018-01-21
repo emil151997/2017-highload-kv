fefd0d5a723737f0e13b816cae630eda6e47a127
Emil151997 <Emil151997@mail.ru>
2018-01-21 03:42:09 +0300

    3rd Stage changes and fixed 1 stage

--- src/main/java/ru/mail/polis/minosyan/MyFileDAO.java
+++ src/main/java/ru/mail/polis/minosyan/MyFileDAO.java
@@ -1,7 +1,7 @@
 package ru.mail.polis.minosyan;
 
 import org.jetbrains.annotations.NotNull;
-
+import gnu.trove.map.hash.THashMap;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,9 +11,10 @@ import java.util.NoSuchElementException;
 public class MyFileDAO implements MyDAO {
     @NotNull
     private final File dir;
-
+    private final THashMap<String,byte[]> Cache;
     public MyFileDAO(@NotNull File dir) {
         this.dir = dir;
+        Cache = new THashMap<>(20000);
     }
 
     private File getFile(@NotNull final String key) {
@@ -24,11 +25,17 @@ public class MyFileDAO implements MyDAO {
     @Override
     public byte[] get(@NotNull final String key) throws NoSuchElementException, IllegalArgumentException, IOException {
         keyCheck(key);
+        byte[] value = Cache.get(key);
+        if (value!=null){
+            return value;
+        }
         Path path = Paths.get(dir.getPath(), key);
         if (Files.notExists(path)) {
             throw new NoSuchElementException();
         }
-        return Files.readAllBytes(path);
+        value = Files.readAllBytes(path);
+        Cache.put(key,value);
+        return Cache.get(key);
     }
 
     @Override
@@ -36,11 +43,13 @@ public class MyFileDAO implements MyDAO {
         try (OutputStream os = new FileOutputStream(getFile(key))) {
             os.write(value);
         }
+        Cache.remove(key);
     }
 
     @Override
     public void delete(@NotNull final String key) throws IllegalArgumentException, IOException {
         getFile(key).delete();
+        Cache.remove(key);
     }
 
     private void keyCheck(String key) {
